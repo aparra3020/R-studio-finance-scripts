042416c295efcc4f204dce114e1f65b7117b3507
Andres Parra <98061167+aparra3020@users.noreply.github.com>
2022-06-22 15:32:02 -0500

    commented

--- BTC MACD.R
+++ BTC MACD.R
@@ -1,24 +1,30 @@
+#required libraries
 library(tidyverse)
 library(tidyquant)
 library(timetk)
 library(highcharter)
-
 library(binancer)
 
+#Daily Candlestick data
 BTC = binance_klines("BTCUSDT", "1d")
 
+#filtering OHLC
 BTC = BTC[,c(1:5)]
 BTC = as_tibble(BTC)
 
+#Transformation to time series format
 BTC_xts = BTC %>% tk_xts(silent = TRUE)
 
+#MACD calculation
 BTCmacd = BTC %>% tq_mutate(select = close, mutate_fun = MACD, nFast= 5, 
                   nSlow      = 20, 
                   nSig       = 5, 
                   maType     = SMA) %>%  mutate(diff = macd - signal) 
 
+#MACD to time series format
 BTCmacd_xts = BTCmacd %>% select(-c(close, open, high, low)) %>% tk_xts(silent = TRUE)
-  
+
+#Interactive BTC chart
 highchart(type = "stock")  %>% 
   hc_yAxis_multiples(
     list(top = "0%", height = "60%", title = list(text = "Precio")),
